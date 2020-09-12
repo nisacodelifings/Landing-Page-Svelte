43da2fe8ed35aa093c9406add3bc2b6b74d49cd8
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-11 20:09:02 -0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Button from "./Button.svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    color: #ffff;
+    margin: 50px;
+  }
+  nav {
+    height: 100vh;
+    width: auto;
+  }
+  @media only screen and (max-width: 768px) {
+    nav {
+      margin-top: 300px;
+    }
+  }
+  img {
+    width: 150px;
+    height: auto;
+  }
+</style>
+
+<main>
+<nav>
+<img src="https://res.cloudinary.com/codelifings/image/upload/v1599879575/code_ziq5za.svg">
+	<h1>Landing Page</h1>
+	<h2>Khusus Untuk Meningkatkan Konversi Penjualan Anda</h2>
+	<Button href="https://api.whatsapp.com/send?phone=6289508320163&text=Saya%20tertarik%20untuk%20Order%20LandingPage.%20Bagaimana%20caranya?" Kata="Order Sekarang" />
+</nav>
+
+  <img src="https://res.cloudinary.com/codelifings/image/upload/v1599877371/sad_tslzdn.svg" />
+  <h1>Jangan Putus Asa Jika Penjualan Anda Masih Sepi</h1><br>
+  <h3>Karena Jasa Pembuatan Landing Page Kami Akan Membantu Anda Merancang Website Penjualan Yang Berkonversi Tinggi</h3>
+  <h2>Terbukti  ampuh dalam MENGHIPNOTIS Pengunjung Untuk Melakukan Apapun Yang Menjadi GOAL Utama Anda…
+Yaitu MENJUAL dan MENGHASILKAN UANG Dari Internet!</h2>
+
+ <img src="https://res.cloudinary.com/codelifings/image/upload/v1599877371/sad_tslzdn.svg" />
+  <h1>Jangan Putus Asa Jika Penjualan Anda Masih Sepi</h1><br>
+  <h3>Karena Jasa Pembuatan Landing Page Kami Akan Membantu Anda Merancang Website Penjualan Yang Berkonversi Tinggi</h3>
+  <h2>Terbukti  ampuh dalam MENGHIPNOTIS Pengunjung Untuk Melakukan Apapun Yang Menjadi GOAL Utama Anda…
+Yaitu MENJUAL dan MENGHASILKAN UANG Dari Internet!</h2>
+
+ <img src="https://res.cloudinary.com/codelifings/image/upload/v1599877371/sad_tslzdn.svg" />
+  <h1>Jangan Putus Asa Jika Penjualan Anda Masih Sepi</h1><br>
+  <h3>Karena Jasa Pembuatan Landing Page Kami Akan Membantu Anda Merancang Website Penjualan Yang Berkonversi Tinggi</h3>
+  <h2>Terbukti  ampuh dalam MENGHIPNOTIS Pengunjung Untuk Melakukan Apapun Yang Menjadi GOAL Utama Anda…
+Yaitu MENJUAL dan MENGHASILKAN UANG Dari Internet!</h2>
+
+ <img src="https://res.cloudinary.com/codelifings/image/upload/v1599877371/sad_tslzdn.svg" />
+  <h1>Jangan Putus Asa Jika Penjualan Anda Masih Sepi</h1><br>
+  <h3>Karena Jasa Pembuatan Landing Page Kami Akan Membantu Anda Merancang Website Penjualan Yang Berkonversi Tinggi</h3>
+  <h2>Terbukti  ampuh dalam MENGHIPNOTIS Pengunjung Untuk Melakukan Apapun Yang Menjadi GOAL Utama Anda…
+Yaitu MENJUAL dan MENGHASILKAN UANG Dari Internet!</h2>
+</main>
---
+++ Button.svelte
@@ -0,0 +1,27 @@
+<style>
+	button {
+	  background: rgb(1, 24, 12);
+	  color: white;
+	  border: none;
+	  padding: 8px 12px;
+	  border-radius: 2px;
+		font-family: "Caveat", cursive;
+		font-size: 20pt;
+		letter-spacing: 1px;
+	}
+	button:hover,
+	button:active {
+	  background: #143b63;
+	}
+</style>
+
+<script>
+	export let href;
+	export let Kata;
+</script>
+
+<a href={href}>
+<button>
+  {Kata}
+</button>
+</a>
